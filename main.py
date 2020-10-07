eb99cb6b47bb565f0f50a7a9b5dbd50308328762
sdi1100041 <manosangelis@gmail.com>
2020-10-07 17:23:58 +0000

    Fix hamming distance

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ previous_acc=0
 ground_truth=0
 previous_test_targets_pred=0
 irr_class=9
+regularization =False
+indices=0
 
 def calculate_correct(predicted,targets):
     global irr_class
@@ -27,17 +29,21 @@ def calculate_correct(predicted,targets):
     return c
 
 def my_cross_entropy(output,targets):
-    global irr_class
+    global irr_class, regularization
     output = nn.LogSoftmax(dim=1)(output)
     logs=torch.zeros(targets.shape[0])
+    c=0
     for i, t in enumerate(targets):
-        if t == irr_class:
+        c+=1
+        if t == irr_class and regularization:
             logs[i]=torch.mean(output[i,:])
+        elif t == irr_class:
+            c-=1
         elif t < irr_class:
             logs[i]=output[i,t]
-        else:
+        elif t > irr_class:
             logs[i]=output[i,t-1]
-    return - torch.mean(logs )
+    return - torch.sum(logs)/float(c)
 
 def hamming_distance(x,y):
     return np.sum(x!=y)
@@ -90,7 +96,7 @@ def train(epoch,net,trainloader,criterion,optimizer):
     #wandb.log({"train_loss":avg_loss,"train_accuracy":avg_accuracy},step=epoch)
 
 def test(epoch,net,testloader,criterion,args):
-    global best_acc,previous_acc,ground_truth,previous_test_targets_pred,irr_class
+    global best_acc,previous_acc,ground_truth,previous_test_targets_pred,irr_class, indices
     net.eval()
     test_loss = 0
     correct = 0
@@ -125,6 +131,7 @@ def test(epoch,net,testloader,criterion,args):
 
     acc=acc/100
     wandb.log({"classification_loss":1-acc},step=epoch)
+    test_targets_pred = test_targets_pred[indices]
 
     if epoch >0:
         wandb.log({"hamming_distance": hamming_distance(previous_test_targets_pred, test_targets_pred)/test_targets_pred.shape[0],"difference":acc - previous_acc },step=epoch)
@@ -148,17 +155,21 @@ def test(epoch,net,testloader,criterion,args):
         best_acc = acc
 
 def construct_and_train(args: dict):
-    global start_epoch,best_acc,ground_truth
+    global start_epoch,best_acc,ground_truth,indices
     trainset = get_train_data(args['task'])
     testset = get_validation_data(args['task'])
     trainloader = torch.utils.data.DataLoader(trainset,batch_size=128, shuffle= False,num_workers=2)
     testloader = torch.utils.data.DataLoader(testset,batch_size=100, shuffle= False,num_workers=1)
     ground_truth=np.array(testset.targets)
+    indices = ground_truth != args['irr_class']
+    ground_truth[ground_truth > irr_class]-=1
+    ground_truth=ground_truth[indices]
 
     net=define_model(args)
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
-    wandb.init(project='optas_irrelevant_class_project', name=args['task']+'_'+args['model']+'_'+args['algorithm']+'_'+str(args['lr']), config=args)
+    prefix = ('REG_OUT_'+str(args['irr_class']) + '_') if args['regularization'] else ('NOT_REG_OUT_'+str(args['irr_class']) + '_')
+    wandb.init(project='optas_irrelevant_class_project', name=prefix + args['task']+'_'+args['model']+'_'+args['algorithm']+'_'+str(args['lr']), config=args)
     wandb.watch(net)
 
     if args['resume']:
@@ -190,7 +201,9 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, choices={'VGG19','ResNet18','GoogLeNet','DPN92'}, help="NN_model")
     parser.add_argument("--epochs",default=20,type=int, choices=range(0,201), help="number of epochs")
     parser.add_argument("--irr_class",default=9,type=int, choices=range(0,10), help="irrelevant class")
+    parser.add_argument( "--regularization", action='store_true', help="use regularization")
     parser.add_argument('--algorithm', type=str, default="SGD", choices={"SGD","Adam"}, help="Optimization algorithm")
     args = vars(parser.parse_args())
     irr_class = args['irr_class']
+    regularization = args['regularization']
     construct_and_train(args)
