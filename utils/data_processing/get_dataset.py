4d3d33679e0d594a005642072ac50f0e464bb720
manos <manosangelis@gmail.com>
2020-10-09 13:02:42 +0300

    Add delete directly

--- utils/data_processing/get_dataset.py
+++ utils/data_processing/get_dataset.py
@@ -27,20 +27,28 @@ class CIFAR10Transform:
                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
 
 
-def get_train_data(task: str):
+def get_train_data(task: str, irr_class: int=-1):
     if task == "MNIST":
-        dataset=  datasets.MNIST(root='../data', train=True, download=True, transform=MNISTTransform.train_transform())
+        dataset=  datasets.MNIST(root='../../data', train=True, download=True, transform=MNISTTransform.train_transform())
     elif task == "EMNIST":
-        dataset=  datasets.EMNIST(root='../data',split="balanced" ,train=True, download=True,transform=EMNISTTransform.train_transform())
+        dataset=  datasets.EMNIST(root='../../data',split="balanced" ,train=True, download=True,transform=EMNISTTransform.train_transform())
     else:# task == "CIFAR10"
-        dataset= datasets.CIFAR10(root='../data', train=True, download=True, transform=CIFAR10Transform.train_transform())
+        dataset= datasets.CIFAR10(root='../../data', train=True, download=True, transform=CIFAR10Transform.train_transform())
+    if irr_class != -1:
+        indices = dataset.targets != irr_class
+        dataset.targets=dataset.targets[indices]
+        dataset.data = dataset.data[indices]
     return dataset
 
-def get_validation_data(task: str):
+def get_validation_data(task: str, irr_class: int=-1):
     if task == "MNIST":
-        dataset=  datasets.MNIST(root='../data', train=False, download=True, transform=MNISTTransform.train_transform())
+        dataset=  datasets.MNIST(root='../../data', train=False, download=True, transform=MNISTTransform.train_transform())
     elif task == "EMNIST":
-        dataset=  datasets.EMNIST(root='../data',split="balanced" ,train=False, download=True,transform=EMNISTTransform.train_transform())
+        dataset=  datasets.EMNIST(root='../../data',split="balanced" ,train=False, download=True,transform=EMNISTTransform.train_transform())
     else:# task == "CIFAR10"
-        dataset= datasets.CIFAR10(root='../data', train=False, download=True, transform=CIFAR10Transform.test_transform())
+        dataset= datasets.CIFAR10(root='../../data', train=False, download=True, transform=CIFAR10Transform.test_transform())
+    if irr_class != -1:
+        indices = dataset.targets != irr_class
+        dataset.targets=dataset.targets[indices]
+        dataset.data = dataset.data[indices]
     return dataset
