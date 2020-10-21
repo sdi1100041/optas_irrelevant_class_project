a3da32da83cd3173d60be2c04b52338459a49eda
sdi1100041 <manosangelis@gmail.com>
2020-10-21 11:25:59 +0000

    Add random shuffling for the combined CIFAR10/100 datasets

--- utils/data_processing/get_dataset.py
+++ utils/data_processing/get_dataset.py
@@ -38,14 +38,18 @@ def get_train_data(task: str):
     else: #task == "CIFAR100"
         dataset1= datasets.CIFAR100(root='../data', train=True, download=True, transform=CIFAR10Transform.train_transform())
         dataset= datasets.CIFAR10(root='../data', train=True, download=True, transform=CIFAR10Transform.train_transform())
-        dataset1.targets= torch.tensor(dataset1.targets)
-        dataset.targets = torch.tensor(dataset.targets)
+        dataset1.targets= np.array(dataset1.targets)
+        dataset.targets = np.array(dataset.targets)
         indices1 = (dataset1.targets != dataset1.class_to_idx['bicycle']) & (dataset1.targets != dataset1.class_to_idx['bus']) & (dataset1.targets != dataset1.class_to_idx['motorcycle']) & (dataset1.targets != dataset1.class_to_idx['pickup_truck']) & (dataset1.targets != dataset1.class_to_idx['train']) & (dataset1.targets != dataset1.class_to_idx['lawn_mower']) & (dataset1.targets != dataset1.class_to_idx['streetcar']) & (dataset1.targets != dataset1.class_to_idx['rocket']) & (dataset1.targets != dataset1.class_to_idx['tank']) & (dataset1.targets != dataset1.class_to_idx['tractor'])
         dataset1.targets=dataset1.targets[indices1]
         dataset1.data = dataset1.data[indices1]
         dataset1.targets[:] = 10
-        dataset.targets = torch.cat([dataset.targets, dataset1.targets])
+        dataset.targets = np.concatenate([dataset.targets, dataset1.targets])
         dataset.data = np.concatenate([dataset.data,dataset1.data])
+        indices = np.arange(dataset.data.shape[0])
+        np.random.shuffle(indices)
+        dataset.targets = dataset.targets[indices]
+        dataset.data = dataset.data[indices]
     return dataset
 
 def get_validation_data(task: str):
